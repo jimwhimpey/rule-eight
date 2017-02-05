90faa3d877b71e632bf9b18203a5111358c2fef8
Jim Whimpey <jwhimpey@slack-corp.com>
2017-02-04 18:31:26 -0800

    Added a rad animation

--- src/App.js
+++ src/App.js
@@ -1,111 +1,12 @@
 import React, { Component } from 'react';
-import WeatherUnit from './WeatherUnit.js';
+import Location from './Location.js';
 import superagent from 'superagent';
-
-var dayToShortDay = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-class Location extends Component {
-
-	constructor(props) {
-		super(props);
-		this.state = {
-			name: props.name,
-			unitsTemp: "\u00B0F",
-			unitsSpeed: "mph",
-			loading: true,
-			latlong: props.latlong,
-			index: props.index
-		};
-		this.upstreamRemove = props.upstreamRemove;
-	}
-
-	componentDidMount() {
-		var component = this;
-		superagent.get("http://" + window.location.host.match(/^(.+):/)[1] + ":3001/" + this.state.latlong).end(function(err, res) {
-			// console.log(res);
-			component.setState({
-				weather: res.body,
-				loading: false,
-				units: component.state.units
-			});
-		});
-	}
-
-	editToggle(e) {
-		if (e.target.className === 'edit') {
-			e.target.className = '';
-		} else {
-			e.target.className = 'edit';
-		}
-	}
-	handleRemove(e) {
-		// Remove this location
-		this.upstreamRemove(this.state.index);
-	}
-
-	render() {
-		if (this.state.loading) {
-			return (<li>
-				<h2>{this.state.name}</h2>
-				<div className="loading">Loading…</div>
-			</li>);
-		} else {
-			return (<li>
-				<h2 onClick={this.editToggle}>
-					{this.state.name}
-					<span onClick={this.handleRemove.bind(this)}>Remove</span>
-				</h2>
-				<ul className="forecast now_and_soon">
-					<li>
-						<h3>Now</h3>
-						<WeatherUnit data={this.state.weather.currently} unitsTemp={this.state.unitsTemp} unitsSpeed={this.state.unitsSpeed} />
-					</li>
-					<li>
-						<h3>+1hr</h3>
-						<WeatherUnit data={this.state.weather.hourly.data[0]} unitsTemp={this.state.unitsTemp} unitsSpeed={this.state.unitsSpeed} />
-					</li>
-					<li>
-						<h3>+2hrs</h3>
-						<WeatherUnit data={this.state.weather.hourly.data[1]} unitsTemp={this.state.unitsTemp} unitsSpeed={this.state.unitsSpeed} />
-					</li>
-					<li>
-						<h3>+4hrs</h3>
-						<WeatherUnit data={this.state.weather.hourly.data[4]} unitsTemp={this.state.unitsTemp} unitsSpeed={this.state.unitsSpeed} />
-					</li>
-					<li>
-						<h3>+8hrs</h3>
-						<WeatherUnit data={this.state.weather.hourly.data[8]} unitsTemp={this.state.unitsTemp} unitsSpeed={this.state.unitsSpeed} />
-					</li>
-				</ul>
-				<ul className="forecast later">
-					<li>
-						<h3>{dayToShortDay[new Date(this.state.weather.daily.data[1].time * 1000).getDay()]}</h3>
-						<WeatherUnit data={this.state.weather.daily.data[1]} unitsTemp={this.state.unitsTemp} unitsSpeed={this.state.unitsSpeed} />
-					</li>
-					<li>
-						<h3>{dayToShortDay[new Date(this.state.weather.daily.data[2].time * 1000).getDay()]}</h3>
-						<WeatherUnit data={this.state.weather.daily.data[2]} unitsTemp={this.state.unitsTemp} unitsSpeed={this.state.unitsSpeed} />
-					</li>
-					<li>
-						<h3>{dayToShortDay[new Date(this.state.weather.daily.data[3].time * 1000).getDay()]}</h3>
-						<WeatherUnit data={this.state.weather.daily.data[3]} unitsTemp={this.state.unitsTemp} unitsSpeed={this.state.unitsSpeed} />
-					</li>
-					<li>
-						<h3>{dayToShortDay[new Date(this.state.weather.daily.data[4].time * 1000).getDay()]}</h3>
-						<WeatherUnit data={this.state.weather.daily.data[4]} unitsTemp={this.state.unitsTemp} unitsSpeed={this.state.unitsSpeed} />
-					</li>
-					<li>
-						<h3>{dayToShortDay[new Date(this.state.weather.daily.data[5].time * 1000).getDay()]}</h3>
-						<WeatherUnit data={this.state.weather.daily.data[5]} unitsTemp={this.state.unitsTemp} unitsSpeed={this.state.unitsSpeed} />
-					</li>
-				</ul>
-			</li>);
-		}
-	}
-}
 
 class App extends Component {
 
+	/**
+	 * Gets the state out of local storage and puts it into the component
+	 */
 	componentDidMount() {
 		this.setState({
 			locations: JSON.parse(localStorage.getItem('locations'))
